6938f098e40949c00ab4c4ed4ec8b5e27f690863
wewececetw <wewececetw@gmail.com>
2024-11-18 11:53:13 +0800

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+// import { apiKey } from "./util";
+
+// import apiKey from "./util";
+
+// import * as result from "./util"
+
+import { apiKey, a2323, car as BWM } from "./util";
+
+console.log(BWM);
---
+++ assets/scripts/util.js
@@ -0,0 +1,8 @@
+// export let apiKey = "a2323232323";
+
+// export default "a2323232323";
+export let apiKey = "a2323232323";
+
+export let a2323 = "12313";
+
+export let car = "BMW";
